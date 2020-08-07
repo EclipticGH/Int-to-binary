4059399da6a6ac72123b069d7d383db84038228f
Dan <40780967+EclipticGH@users.noreply.github.com>
2020-08-07 12:07:07 -0700

    Add files via upload

---
+++ Binary/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Binary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Convert int to binary";
+            try
+            {
+                string val;
+                int output;
+                Console.WriteLine("Enter a number too convert to binary: ");
+                val = Console.ReadLine();
+                output = Convert.ToInt32(val);
+
+                string binary = Convert.ToString(output, 2);
+                Console.WriteLine(binary);
+                Console.ReadKey(); //Stops program from stopping until a key is pressed
+            }
+            catch(Exception e)
+            {
+                Console.Clear();
+                Console.WriteLine(e.Message);
+                Task.Delay(10000);
+            };
+        }
+    }
+}
